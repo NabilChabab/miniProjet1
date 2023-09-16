80f6379264e57b676085857efdc3315ded63c5e8
nabilchabab <nabil.chababnabil@gmail.com>
2023-09-16 15:55:43 +0100

    MiniProjet

--- problemSolving1.c
+++ problemSolving1.c
@@ -34,18 +34,135 @@ int racinecarre(int a){
     return 0;
 }
 
+int max(){
+
+    int nTab[100];
+    int number , i ;
+    int m ;
+
+
+    printf("Entrer le nombre des elements : ");
+    scanf("%d",&number);
+
+    printf("Entrer les elements : { \n",number);
+
+    for (i = 0 ; i < number ; i++){
+        scanf("%d",&nTab[i]);
+    }
+    for (int i = 0; i < number ; i++){
+        if (nTab[0] < nTab[i]){
+            nTab[0] = nTab[i];
+        }
+        else{
+        }
+        
+    }
+    printf("Le Max de Ce Tableaux est :%d\n",nTab[0]);
+    
+    return 0;
+    
+}
+
+int min(){
+
+    int nTab[100];
+    int number , i ;
+    int m ;
+
+
+    printf("Entrer le nombre des elements : ");
+    scanf("%d",&number);
+
+    printf("Entrer les elements : { \n",number);
+
+    for (i = 0 ; i < number ; i++){
+        scanf("%d",&nTab[i]);
+    }
+    for (int i = 0; i > number ; i++){
+        if (nTab[0] < nTab[i]){
+            nTab[0] = nTab[i];
+        }
+        else{
+        }
+        
+    }
+    printf("Le Min de Ce Tableaux est :%d\n",nTab[0]);
+    return 0;
+}
+
+int moyenne(){
+    int nTab[100];
+    int number , i ;
+    int m ;
+
+
+    printf("Entrer le nombre des elements : ");
+    scanf("%d",&number);
+
+    printf("Entrer les elements : { \n",number);
+
+    for (i = 0 ; i < number ; i++){
+        scanf("%d",&nTab[i]);
+    }
+    for (int i = 0; i < number ; i++){
+        if (nTab[0] < nTab[i]){
+            int res = nTab[0] + nTab[i];
+        }
+        else{
+        }
+        
+    }
+    printf("Le Moyenne de Ce Tableaux est :%d\n",nTab[0]/number);
+    return 0;
+}
+
+int MenuTableaux(){
+
+    int choixM;
+    printf("*************************Choisissez une autre operation***************************************\n");
+    printf("1. Le Max : ");
+    printf("\n2. Le Min : ");
+    printf("\n3. Le Moyenne : \n");
+    printf("le Choix : ");
+    scanf("%d",&choixM);
+
+
+    switch (choixM){
+    case 1:
+        printf("%d\n",max());
+        break;
+    case 2:
+        printf("%d\n",min());
+        break;
+    case 3:
+        printf("%d\n",moyenne());
+        break;
+    }
+
+    return 0;
+}
+
+int Tableaux(){
+
+    MenuTableaux();
+
+    return 0;
+}
+
 void menu(){
     int menu;
     int n , n2;
 
     do{
 
-        printf("*************************************\n");
+        printf("************************************\n");
         printf("Entrer la valeur de a :");
         scanf("%d",&n);
         printf("Entrer la valeur de b :");
         scanf("%d",&n2);
+        
         printf("*************************************\n");
+        printf("Le Menu : \n");
         printf("Choisissez une operation :");
         printf("\n1. Addition:");
         printf("\n2. Soustraction :");
@@ -53,9 +170,11 @@ void menu(){
         printf("\n4. Division :");
         printf("\n5. Carre :");
         printf("\n6. Racine Carre :");
-        printf("\n7. Quitter :\n");
+        printf("\n7. Tableaux :");
+        printf("\n8. Quitter :\n");
+        printf("le Choix : ");
         scanf("%d",&menu);
-        printf("************************************\n");
+
 
         
 
@@ -79,6 +198,9 @@ void menu(){
             printf("%d\n",racinecarre(n));
             break;
         case 7:
+            printf("%d\n",Tableaux());
+            break;    
+        case 8:
             printf("coa!!!!!!!!!!!!!!");
             break;                            
        
